3e3b222ce33890fe459ae1ee53d37ceb085d9b2a
Hector Lemus <dev.hectorlemus@gmail.com>
2020-06-26 16:40:02 -0500

    Created 'organization' model and 'image_url' property added to 'trash' model

--- garbagecollector/admin.py
+++ garbagecollector/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Trash
+from .models import Organization, Trash
 
 
 admin.site.register(Trash)
+admin.site.register(Organization)
--- garbagecollector/models.py
+++ garbagecollector/models.py
@@ -1,9 +1,28 @@
 from django.db import models
 
 
+class Organization(models.Model):
+    creation_date = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100)
+    operation_area = models.CharField(max_length=100)
+    phone_one = models.CharField(max_length=12)
+    phone_two = models.CharField(max_length=12)
+    address = models.CharField(max_length=100)
+    manager_name = models.CharField(max_length=100)
+    manager_phone = models.CharField(max_length=12)
+    manager_email = models.EmailField()
+
+    def __str__(self):
+            return self.name
+
+    class Meta:
+        verbose_name_plural = "Organizations"
+
+
 class Trash(models.Model): 
     creation_date = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=100)
+    image_url = models.CharField(max_length=250, null=True)
 
     def __str__(self):
         return self.name
