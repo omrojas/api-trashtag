919d97c3c1e64a656b17637c83e504cb1955ab8d
Hector Lemus <dev.hectorlemus@gmail.com>
2020-08-02 11:15:10 -0500

    Functionality is added to obtain user statistics.

--- garbagecollector/graphql/query.py
+++ garbagecollector/graphql/query.py
@@ -1,15 +1,17 @@
 import graphene
+from django.db.models import Min, Sum
 from graphql_jwt.decorators import login_required
 
-from garbagecollector.models import Organization, Trash
+from garbagecollector.models import Level, Organization, Trash, TrashCleanup
 
-from .types import OrganizationType, TrashType, UserType
+from .types import OrganizationType, TrashType, UserStatistics, UserType
 
 
 class Query(object):
     all_trashes = graphene.List(TrashType)
     all_organizations = graphene.List(OrganizationType)
     user_information = graphene.Field(UserType)
+    user_statistics = graphene.Field(UserStatistics)
 
     @login_required
     def resolve_all_trashes(self, info, **kwargs):
@@ -29,5 +31,25 @@ class Query(object):
         try:
             user = info.context.user
             return user
+        except Exception as e:
+            return None
+
+    @login_required
+    def resolve_user_statistics(self, info, **kwargs):
+        try:
+            user = info.context.user
+            cleanups = TrashCleanup.objects.filter(cleanup__user=user).count()
+            itemsPicked = TrashCleanup.objects.filter(cleanup__user=user).aggregate(Sum('quantity'))['quantity__sum']
+            nextLevelCleanups= Level.objects.filter(cleanups__gte=itemsPicked).aggregate(Min('cleanups'))['cleanups__min']
+            itemsToNextLevel = 0
+
+            if nextLevelCleanups is not None:
+                itemsToNextLevel = nextLevelCleanups - itemsPicked
+
+            return {
+                'cleanups': cleanups,
+                'itemsPicked': itemsPicked,
+                'itemsToNextLevel': itemsToNextLevel
+            }
         except Exception as e:
             return None
--- garbagecollector/graphql/types.py
+++ garbagecollector/graphql/types.py
@@ -25,3 +25,19 @@ class OrganizationType(DjangoObjectType):
 class TrashQuantity(graphene.InputObjectType):
     trashId = graphene.Int(required=True)
     quantity = graphene.Int(required=True)
+
+
+class UserStatistics(graphene.ObjectType):
+    cleanups = graphene.Int()
+    items_picked = graphene.Int()
+    items_to_next_level = graphene.Int()
+
+    def resolve_cleanups(statistics, info):
+        return statistics['cleanups']
+
+    def resolve_items_picked(statistics,info):
+        return statistics['itemsPicked']
+
+    def resolve_items_to_next_level(statistics,info):
+        return statistics['itemsToNextLevel']
+
